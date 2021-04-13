fb992f6bf33814ce2703267be7ac0caed4111667
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-04-13 10:56:08 +0100

    Corrected initial comment

--- list-org-repos.py
+++ list-org-repos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Python helper to dump labels from a Github repo
+# Python helper to list repos from a Github org
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
